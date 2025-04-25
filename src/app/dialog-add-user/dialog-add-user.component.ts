6f22672dd9ce84162a4076ad42f7e76db1c45f92
Susanne714 <sambale.su@gmail.com>
2025-04-25 11:14:28 +0200

    added progress bar behaviour to saving function

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FirestoreService } from '../services/firestore.service';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
@@ -6,6 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
 
@@ -13,13 +15,14 @@ import { User } from '../../models/user.class';
   selector: 'app-dialog-add-user',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [FormsModule, MatDialogActions, MatDialogContent, MatDialogModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatDatepickerModule],
+  imports: [CommonModule, FormsModule, MatDialogActions, MatDialogContent, MatDialogModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatDatepickerModule, MatProgressBarModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
   user = new User();
   birthDate: Date = new Date();
+  loading = false;
 
   constructor(private firestoreService: FirestoreService) { }
 
@@ -40,9 +43,17 @@ export class DialogAddUserComponent {
 
   saveUser() {
     this.user.birthDate = this.birthDate.getTime();
+    this.loading = true;
+
     this.firestoreService.addData('users', this.user.toPlainObject())
-      .then(() => console.log('User erfolgreich hinzugefügt!'))
-      .catch(error => console.error('Fehler beim Hinzufügen des Users:', error));
+      .then(() => {
+        console.log('User erfolgreich hinzugefügt!');
+        this.loading = false; // Progressbar wieder ausblenden
+      })
+      .catch(error => {
+        console.error('Fehler beim Hinzufügen des Users:', error);
+        this.loading = false; // Auch im Fehlerfall ausschalten
+      });
   }
 
 }
