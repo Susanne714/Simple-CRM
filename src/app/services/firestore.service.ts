caff3c436493548a691ab2b14ebf3e1b24a221c1
Susanne714 <sambale.su@gmail.com>
2025-04-28 18:12:46 +0200

    new method to retrieve single user data

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { addDoc, collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { doc, getDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +20,16 @@ export class FirestoreService {
   addData(collectionName: string, data: any) {
     const dataCollection = collection(this.firestore, collectionName);
     return addDoc(dataCollection, data);
+  }
+
+  getSingleUser(collectionName: string, id: string): Promise<any> {
+    const documentReference = doc(this.firestore, collectionName, id);
+    return getDoc(documentReference).then(docSnap => {
+      if (docSnap.exists()) {
+        return docSnap.data();
+      } else {
+        throw new Error('Dokument existiert nicht')
+      }
+    })
   }
 }
