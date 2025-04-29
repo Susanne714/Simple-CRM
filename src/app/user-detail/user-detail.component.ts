372ab40f31f42ce5453b56fe2bc18b351778d946
Susanne714 <sambale.su@gmail.com>
2025-04-29 12:17:31 +0200

    opening functions added to menu buttons

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { ActivatedRoute } from '@angular/router';
 import { FirestoreService } from '../services/firestore.service';
 import { User } from '../../models/user.class';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
+import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
@@ -17,7 +23,7 @@ export class UserDetailComponent implements OnInit {
   userID: string = '';
   userData: User = new User();
 
-  constructor(private route: ActivatedRoute, private firestoreService: FirestoreService) { }
+  constructor(private route: ActivatedRoute, private firestoreService: FirestoreService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -39,6 +45,13 @@ export class UserDetailComponent implements OnInit {
     }
   }
 
+  editUserDetail() {
+    this.dialog.open(DialogEditUserComponent);
+  }
+
+  editMenu() {
+    this.dialog.open(DialogEditAddressComponent);
+  }
 
 
 }
